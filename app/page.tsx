366e1ab4431554880a239eeccd7e7183a88cc56f
okiesfirmansyah <70733533+okiesfirmansyah@users.noreply.github.com>
2025-11-11 19:58:05 +0700

    Feat: Add initial Next.js app/page structure

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// Ini adalah halaman "Home" Anda
+export default function HomePage() {
+    return (
+      <div>
+        <h1>Selamat Datang!</h1>
+        <p>Proyek Next.js saya sudah berhasil di-deploy.</p>
+      </div>
+    );
+  }
